3ed55fedf5f60d66ea874f26db03bf48da031651
Marlo Ito <email": null>
2017-10-11 17:27:49 -0700

    user can create an account -- user error handling done

---
+++ app/controllers/users.rb
@@ -0,0 +1,17 @@
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    redirect "/users/#{@user.id}"
+  else
+    @errors = @user.errors.full_messages
+    erb :'/users/new'
+  end
+end
+
+get '/users/new' do
+  erb :'/users/new'
+end
+
+get '/users/:id' do
+  'made it!'
+end
