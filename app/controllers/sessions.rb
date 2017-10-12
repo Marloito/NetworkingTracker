01004e361b2936b495b7a32cd3ca7ba999eda056
Marlo Ito <email": null>
2017-10-11 19:09:42 -0700

    user can login!

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -3,7 +3,7 @@ get '/sessions/new' do
 end
 
 post '/sessions' do
-  user = User.authenticate(params[:username], params[:password])
+  user = User.authenticate(params[:email], params[:password])
   if user
     session[:user_id] = user.id
     redirect '/'
