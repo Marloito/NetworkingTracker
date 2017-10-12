8241839c76e9c2397cd8d0908cb5c48a0030c0f4
Marlo Ito <email": null>
2017-10-11 17:33:53 -0700

    adds redirect to new session form

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,7 +1,7 @@
 post '/users' do
   @user = User.new(params[:user])
   if @user.save
-    redirect "/users/#{@user.id}"
+    redirect "/sessions/new"
   else
     @errors = @user.errors.full_messages
     erb :'/users/new'
