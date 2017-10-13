ea8246a05115b5048f3fe0cbe89ea609e410007c
Marlo Ito <email": null>
2017-10-13 12:04:27 -0700

    contacts show page done

--- app/controllers/contacts.rb
+++ app/controllers/contacts.rb
@@ -1,4 +1,10 @@
 get '/users/:user_id/contacts' do
   @user = User.find(session[:user_id])
-  'users contacts index'
+  @contacts = @user.contacts.all
+  erb :'/contacts/index'
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  erb :'/contacts/show'
 end
