29c0987ae3d9e072b7f4dca021f1713d6a4b3314
Marlo Ito <email": null>
2017-10-13 12:14:01 -0700

    sorts contacts by alphabetical order

--- app/controllers/contacts.rb
+++ app/controllers/contacts.rb
@@ -1,6 +1,7 @@
 get '/users/:user_id/contacts' do
   @user = User.find(session[:user_id])
   @contacts = @user.contacts.all
+  @contacts = @contacts.sort { |a,b| a.name.downcase <=> b.name.downcase }
   erb :'/contacts/index'
 end
 
